4022a2f4494c327a4500bde0253e2cfe010a27f4
jonchurch <me@jonchurch.com>
2016-05-06 20:22:15 -0400

    trying to get bot to respond to controller.hears on line 92 index.js

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ controller.hears('hello', 'direct_message', function (bot, message) {
     bot.reply(message, 'Hello!');
 });
 
-controller.hears('lunch run', 'message_received', function(bot, message) {
+controller.hears('lunch run', 'direct_message, direct_mention, message_received', function(bot, message) {
     
         bot.reply(message, "Oooh! I LOVE Pop-a-Top!");
     
