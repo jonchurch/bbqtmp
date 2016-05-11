5331ac6cbf9cd221254b4532e9cfd280d77fa445
Jon Church <me@jonchurch.com>
2016-05-11 16:04:46 -0400

    pizza

--- index.js
+++ index.js
@@ -191,7 +191,7 @@ controller.hears('(.*)', 'direct_mention, direct_message', function(bot, message
 function gatherRequest(bot, message) {
     console.log('===request = ' + message);
     bot.reply(message, 'Got it! Thanks ' + message.user);
-    runList.push({user: message.user, request: message.text});
+    runList.push(message.user + message.text);
     console.log('Pushing request to runList');
 
    /* runModel.addRequest({
